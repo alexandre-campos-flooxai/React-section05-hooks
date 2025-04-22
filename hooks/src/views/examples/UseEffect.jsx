29db76fb934f89c759fdb29e90ec71d91bd93108
AlexandreCampos <alexandre.campos.flooxai@gmail.com>
2025-04-22 10:32:15 -0300

    feat: exemple useState and useEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,15 +1,70 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+import React, { useState, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
+//ex01
+function calcFatorial(num) {
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  useEffect(
+    function () {
+      setFatorial(calcFatorial(number));
+    },
+    [number]
+  );
+
+  useEffect(function () {
+    if (fatorial > 1000) {
+      document.title = "Eita!";
+    }
+  });
+
+  //ex02
+  useEffect(
+    function () {
+      setStatus(number % 2 === 0 ? "Par" : "Ímpar");
+    },
+    [number]
+  );
+  const [status, setStatus] = useState("Ímpar");
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <div>
+          <span className="text">Fatorial:</span>
+          <span className="text red">
+            {fatorial === -1 ? "Não existe" : fatorial}
+          </span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <div>
+          {" "}
+          <span className="text">Status:</span>
+          <span className="text red">{status}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
