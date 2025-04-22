3b687e583f813db71d3ca50ce6b5a962881c9356
AlexandreCampos <alexandre.campos.flooxai@gmail.com>
2025-04-22 09:24:36 -0300

    feat: configuration project

---
+++ hooks/src/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+import App from "./views/App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
