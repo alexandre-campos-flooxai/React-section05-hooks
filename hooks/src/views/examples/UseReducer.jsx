f69a8a9e1e492b7202d7d9f4bbaa9b6709a1021e
AlexandreCampos <alexandre.campos.flooxai@gmail.com>
2025-04-22 15:10:48 -0300

    feat: userReducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,15 +1,88 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from "react";
+import { useReducer } from "react";
 
-const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-        </div>
-    )
+import PageTitle from "../../components/layout/PageTitle";
+
+const initialState = {
+  card: [],
+  products: [],
+  user: null,
+  //foco...
+  number: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "numberAdd2":
+      return { ...state, number: state.number + 2 };
+    case "numberMulti7":
+      return { ...state, number: state.number * 7 };
+    case "numberDiv25":
+      return { ...state, number: state.number / 25 };
+    case "numberInt":
+      return { ...state, number: parseInt(state.number) };
+    case "numberAddN":
+      return { ...state, number: state.number + action.payload };
+    case "login":
+      return { ...state, user: { name: action.payload } };
+    default:
+      return state;
+  }
 }
 
-export default UseReducer
+const UseReducer = (props) => {
+  const [state, exec] = useReducer(reducer, initialState);
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem usuário</span>
+        )}{" "}
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => exec({ type: "login", payload: "Alex" })}
+          >
+            Login
+          </button>
+          <button className="btn" onClick={() => exec({ type: "numberAdd2" })}>
+            +2
+          </button>
+          <button
+            className="btn"
+            onClick={() => exec({ type: "numberMulti7" })}
+          >
+            *7
+          </button>
+          <button className="btn" onClick={() => exec({ type: "numberDiv25" })}>
+            /25
+          </button>
+          <button className="btn" onClick={() => exec({ type: "numberInt" })}>
+            Int
+          </button>{" "}
+          <button
+            className="btn"
+            onClick={() => exec({ type: "numberAddN", payload: -9 })}
+          >
+            -9
+          </button>{" "}
+          <button
+            className="btn"
+            onClick={() => exec({ type: "numberAddN", payload: +11 })}
+          >
+            +11
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseReducer;
